0f788218303339f61fe1e139c5d13aa389a545d2
Dhyey Kathiriya <dhk22500@gmail.com>
2023-03-30 23:32:48 +0530

    Fixed Bootstrap structure, made tables look good!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import { Route, Link, useNavigate, Routes, Router, BrowserRouter } from 'react-router-dom';
-import './App.css'
-import Entry from './Components/Entry';
-import Navbar from './Components/Navbar';
-import searchIcon from './search.svg';
-import Subject from './Subject';
+import React, { useEffect, useState, useMemo } from "react";
+import {
+  Route,
+  Link,
+  useNavigate,
+  Routes,
+  Router,
+  BrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import Entry from "./Components/Entry";
+import Navbar from "./Components/Navbar";
+import searchIcon from "./search.svg";
+import Subject from "./Subject";
 
 const HomePage = () => {
   const [books, setBooks] = useState([]);
@@ -14,13 +21,13 @@ const HomePage = () => {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState();
   const [offset, setOffset] = useState(0);
-  const [searchResults, setSearchResults] = useState([]);;
+  const [searchResults, setSearchResults] = useState([]);
 
   const memoizedBooks = useMemo(() => books, [books]);
 
   useEffect(() => {
-    searchBooks()
-  }, [offset])
+    searchBooks();
+  }, [offset]);
 
   const searchBooks = async (term) => {
     if (searchTerm === "") return;
@@ -33,115 +40,149 @@ const HomePage = () => {
       const res = await response.json();
 
       setLoading(false);
-      setErr(false)
-      setTotalPages(Math.ceil((res.numFound) / 10))
-      res.numFound !== 0 ? setBooks(res.docs) : setErr(true)
+      setErr(false);
+      setTotalPages(Math.ceil(res.numFound / 10));
+      res.numFound !== 0 ? setBooks(res.docs) : setErr(true);
     } catch (e) {
-      console.log(e)
+      console.log(e);
       setErr(true);
       setLoading(false);
     }
   };
 
-  const customStyle1 = {
-    display: "flex",
-    marginTop: "20px"
-  }
-
-  const customStyle2 = {
-    alignSelf: "center",
-    margin: "10px",
-    color: "#1D3557"
-  }
-
   return (
     <div className="app">
-
       <Navbar />
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1
+              class="mx-auto "
+              style={{ cursor: "pointer", textAlign: "center" }}
+              onClick={() => {
+                setSearchTerm("");
+                setBooks([]);
+              }}
+            >
+              BookCheff
+            </h1>
+            <h4
+              class="mx-auto"
+              style={{ color: "dimgray", textAlign: "center" }}
+            >
+              Your personal library of books
+            </h4>
+          </div>
+          <div class="col-12">
+            <div className="search w-100">
+              <input
+                class="mx-auto"
+                placeholder="Search for books by title, author.."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key == "Enter") {
+                    searchBooks(setSearchTerm);
+                    setPage(1);
+                    setOffset(0);
+                  }
+                }}
+              />
+              {searchTerm !== "" && (
+                <button
+                  style={{ marginRight: "2%" }}
+                  onClick={() => {
+                    setSearchTerm("");
+                    setBooks([]);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-x"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                  </svg>
+                </button>
+              )}
+              <img
+                src={searchIcon}
+                alt="search"
+                onClick={() => {
+                  searchBooks(searchTerm);
+                  setOffset(0);
+                  setPage(1);
+                  setBooks([]);
+                }}
+              />
+            </div>
+          </div>
+        </div>
 
-      <h1
-        style={{ cursor: "pointer" }}
-        onClick={() => {
-          setSearchTerm("");
-          setBooks([])
-        }}
-      >
-        BookCheff
-      </h1>
-      <h4 style={{ color: "dimgray" }}>Your personal library of books</h4>
+        {/* BOOKS TABLE */}
 
-      <div className="search">
-        <input
-          placeholder="Search for books by title, author.."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key == 'Enter') {
-              searchBooks(setSearchTerm);
-              setPage(1);
-              setOffset(0)
-            }
-          }}
-        />
-        {searchTerm !== "" && (<button style={{ marginRight: "2%" }} onClick={() => {
-          setSearchTerm("");
-          setBooks([])
-        }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-          </svg>
-        </button>)}
-        <img
-          src={searchIcon}
-          alt="search"
-          onClick={() => {
-            searchBooks(searchTerm);
-            setOffset(0)
-            setPage(1)
-            setBooks([]);
-          }}
-        />
+        {err === true && loading === false && searchTerm !== "" && (
+          <h2>Oh No! Looks like there's some issue :(</h2>
+        )}
+        {loading === true && (
+          <span style={{ margin: "0% 20%" }} class="loader"></span>
+        )}
+        {memoizedBooks.length > 0 && loading === false && err === false && (
+          <div>
+            <div class="row">
+              <div class="col">
+                <table class="table table-bordered table-responsive">
+                  <thead>
+                    <tr>
+                      <th>Title</th>
+                      <th>Author</th>
+                      <th>First Published</th>
+                      <th>Edition Cout</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {memoizedBooks.map((book) => {
+                      return <Entry key={book.key} {...book} />;
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center mb-4">
+                <button
+                  className="btn btn-secondary mx-2"
+                  onClick={() => {
+                    if (page > 1) {
+                      setPage(page - 1);
+                      setOffset(offset - 10);
+                    }
+                  }}
+                >
+                  PREV
+                </button>
+                <span class="p-1">
+                  Page {page} of {totalPages}
+                </span>
+                <button
+                  className="btn btn-secondary mx-2"
+                  onClick={() => {
+                    if (page < totalPages) {
+                      setPage(page + 1);
+                      setOffset(offset + 10);
+                    }
+                  }}
+                >
+                  NEXT
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-
-      {/* BOOKS TABLE */}
-
-      {err === true && loading === false && searchTerm !== "" && <h2>Oh No! Looks like there's some issue :(</h2>}
-      {loading === true && <span style={{ margin: "0% 20%" }} class="loader"></span>}
-
-      {memoizedBooks.length > 0 && loading === false && err === false && (
-        <>
-          <div style={customStyle1}>
-            <button className='btn btn-secondary' style={{ margin: "10px" }} onClick={() => { if (page > 1) { setPage(page - 1); setOffset(offset - 10) } }}>PREV</button>
-            <p style={customStyle2}>Page {page} of {totalPages}</p>
-            <button className='btn btn-secondary' style={{ margin: "10px" }} onClick={() => { if (page < totalPages) { setPage(page + 1); setOffset(offset + 10) } }}>NEXT</button>
-          </div>
-
-          <table style={{ margin: "2% auto" }}>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>First Published</th>
-                <th>Edition Cout</th>
-              </tr>
-            </thead>
-            <tbody>
-              {memoizedBooks.map((book) => {
-                return <Entry key={book.key} {...book} />;
-              })}
-            </tbody>
-          </table>
-
-          <div style={customStyle1}>
-            <button className='btn btn-secondary' style={{ margin: "10px" }} onClick={() => { if (page > 1) { setPage(page - 1); setOffset(offset - 10) } }}>PREV</button>
-            <p style={customStyle2}>Page {page} of {totalPages}</p>
-            <button className='btn btn-secondary' style={{ margin: "10px" }} onClick={() => { if (page < totalPages) { setPage(page + 1); setOffset(offset + 10) } }}>NEXT</button>
-          </div>
-
-        </>
-      )
-      }
-
     </div>
   );
 };
@@ -151,11 +192,11 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/physics" element={<Subject name='Physics' />} />
-        <Route path="/maths" element={<Subject name='Mathematics' />} />
-        <Route path="/philosophy" element={<Subject name='Philosophy' />} />
-        <Route path="/history" element={<Subject name='History' />} />
-        <Route path="/sanskrit" element={<Subject name='Sanskrit' />} />
+        <Route path="/physics" element={<Subject name="Physics" />} />
+        <Route path="/maths" element={<Subject name="Mathematics" />} />
+        <Route path="/philosophy" element={<Subject name="Philosophy" />} />
+        <Route path="/history" element={<Subject name="History" />} />
+        <Route path="/sanskrit" element={<Subject name="Sanskrit" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,37 +1,70 @@
 import React from "react";
-import { Route, Link, useNavigate, Routes, Router, BrowserRouter } from 'react-router-dom';
+import {
+  Route,
+  Link,
+  useNavigate,
+  Routes,
+  Router,
+  BrowserRouter,
+} from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <nav style={{ width: "100%", marginBottom: "3%" }} class="navbar navbar-dark navbar-expand-lg subjects">
-            <a class="navbar-brand subjects-title" onClick={() => navigate('/')}>Popular Subjects</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item subject physics" onClick={() => navigate('/physics')}>
-                        <a class="nav-link subject-name">Physics</a>
-                    </li>
-                    <li class="nav-item subject maths" onClick={() => navigate('/maths')}>
-                        <a class="nav-link subject-name">Mathematics</a>
-                    </li>
-                    <li class="nav-item subject philosophy" onClick={() => navigate('/philosophy')}>
-                        <a style={{ color: "#1D3557" }} class="nav-link">Philosophy</a>
-                    </li>
-                    <li class="nav-item subject history" onClick={() => navigate('/history')}>
-                        <a class="nav-link subject-name">History</a>
-                    </li>
-                    <li class="nav-item subject sanskrit" onClick={() => navigate('/sanskrit')}>
-                        <a class="nav-link subject-name">Sanskrit</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav
+      style={{ width: "100%" }}
+      class="navbar navbar-dark navbar-expand-lg subjects"
+    >
+      <a class="navbar-brand subjects-title" onClick={() => navigate("/")}>
+        Popular Subjects
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li
+            class="nav-item subject physics"
+            onClick={() => navigate("/physics")}
+          >
+            <a class="nav-link subject-name">Physics</a>
+          </li>
+          <li class="nav-item subject maths" onClick={() => navigate("/maths")}>
+            <a class="nav-link subject-name">Mathematics</a>
+          </li>
+          <li
+            class="nav-item subject philosophy"
+            onClick={() => navigate("/philosophy")}
+          >
+            <a style={{ color: "#1D3557" }} class="nav-link">
+              Philosophy
+            </a>
+          </li>
+          <li
+            class="nav-item subject history"
+            onClick={() => navigate("/history")}
+          >
+            <a class="nav-link subject-name">History</a>
+          </li>
+          <li
+            class="nav-item subject sanskrit"
+            onClick={() => navigate("/sanskrit")}
+          >
+            <a class="nav-link subject-name">Sanskrit</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
